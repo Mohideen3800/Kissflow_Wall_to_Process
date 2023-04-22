7017eeccdfb161628a2845036567bca64d849ef1
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2023-04-22 07:27:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const lcnc = new LCNC();
+const lcncSDK = require('lcnc-sdk-js'); // Import the lcnc-sdk-js library
 
-function getProfileInfo() {
-    lcnc.api("/id").then((res) => {
-        console.info("API response is", res);
-        document.getElementById("output").innerText = res.UserDetails.Name;
-     });
-}
+// Authenticate with the Kissflow API
+const apiKey = 'YOUR_API_KEY';
+const apiBaseUrl = 'https://YOUR_DOMAIN.kissflow.com/api';
 
-window.onload = function onLoad() {
-    if (!lcnc) {
-        lcnc = new LCNC();
-    }
-    watchParam();
-}
+// Retrieve the messages from the wall
+const wallId = 'WALL_ID';
+const wallMessagesUrl = `${apiBaseUrl}/v1/walls/${wallId}/messages`;
+const wallMessagesResponse = await lcncSDK.get(wallMessagesUrl, apiKey); // Use the lcnc-sdk-js to retrieve the messages
+const wallMessages = wallMessagesResponse.data;
 
-function watchParam() {
-    lcnc.watchParams(function (data) {
-        console.log("watch params data", data);
-        document.getElementById("output").innerText = JSON.stringify(data);
-    });
-}
+// Get the content of the latest message
+const latestMessage = wallMessages[0];
+const latestMessageContent = latestMessage.content;
+
+// Trigger a new process in Kissflow
+const domain = 'YOUR_DOMAIN';
+const app = 'YOUR_APP_NAME';
+const flow = 'YOUR_FLOW_NAME';
+const event = 'YOUR_EVENT_NAME';
+const createProcessUrl = `${apiBaseUrl}/1.1/${domain}/${app}/${flow}/${event}`;
+const createProcessData = {
+  input: {
+    message: latestMessageContent, // Pass the latest message content as input to the process
+  },
+};
+const createProcessResponse = await lcncSDK.post(createProcessUrl, createProcessData, apiKey); // Use the lcnc-sdk-js to trigger the process
+
+console.log('New process created with message content:', latestMessageContent);
